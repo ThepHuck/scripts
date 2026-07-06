515b62ae5c4d81e45f5d0898185185ee74beb928
ThepHuck <luke@thephuck.com>
2026-07-06 08:45:33 -0500

    combined entropy, API CHV, and CPU brand for 9.1 labs on AMD, per https://williamlam.com/2026/05/vcf-9-1-comprehensive-esx-configuration-workarounds-for-lab-deployments.html

--- fix-amd.ps1
+++ fix-amd.ps1
@@ -1,7 +1,9 @@
 #requires -Modules posh-ssh 
 $vmhosts = @("node1","node2","node3","node4")
 $creds = Get-Credential
-$cmd = "echo 'cpuid.brandstring = `"AMD EPYC Ryzen 9 7945HX`"' >> /etc/vmware/config"
+$brandstringcmd = "echo 'cpuid.brandstring = `"AMD EPYC Ryzen 9 7945HX`"' >> /etc/vmware/config"
+$apichvcmd = "echo 'monitor_control.disable_apichv =`"TRUE`"' >> /etc/vmware/config"
+
 foreach ($i in $vmhosts){
     write-host -fore green `n`t "Connecting to $i"   
     connect-viserver $i -credential $creds
@@ -18,7 +20,20 @@ foreach ($i in $vmhosts){
 
     write-host -fore green `n`t "SSHing into the host to set cpuid.brandstring"
     $ssh = New-SSHSession -computername $i -credential $creds -Force -KeepAliveInterval 5 -Verbose -WarningAction SilentlyContinue
-    Invoke-SSHCommand -SessionId $ssh.SessionId -Command $cmd -TimeOut 30
+    $brandstringresult = Invoke-SSHCommand -SessionId $ssh.SessionId -Command $brandstringcmd -TimeOut 30
+    if ($brandstringresult.ExitStatus -eq 0) {
+            Write-Host "Brand string command executed successfully on $i." -ForegroundColor Green
+        } else {
+            Write-Host "Brand string command execution failed on $i. Exit status: $($brandstringresult.ExitStatus)" -ForegroundColor Red
+        }
+    
+    $apichvresult = Invoke-SSHCommand -SessionId $ssh.SessionId -Command $apichvcmd -TimeOut 30
+    if ($apichvresult.ExitStatus -eq 0) {
+        Write-Host "API CHV command executed successfully on $i." -ForegroundColor Green
+    } else {
+        Write-Host "API CHV command execution failed on $i. Exit status: $($apichvresult.ExitStatus)" -ForegroundColor Red
+    }
+
     Remove-SSHSession -SessionId $ssh.SessionId
 
     write-host -fore green `n`t "Rebooting $i"
